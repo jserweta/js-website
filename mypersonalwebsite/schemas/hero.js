5b6d917f7af608514b7af93635ef226e28d441ad
jserweta <jakub@kiwwwi.pl>
2022-02-27 00:33:22 +0100

    Prepare project template with sanity

---
+++ mypersonalwebsite/schemas/hero.js
@@ -0,0 +1,23 @@
+export default {
+  name: "hero",
+  title: "Hero",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+    },
+    {
+      name: "date",
+      type: "datetime",
+    },
+    {
+      name: "place",
+      type: "string",
+    },
+    {
+      name: "description",
+      type: "text",
+    }
+  ]
+}
