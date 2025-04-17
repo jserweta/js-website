9c4530e6d7429bef7269c609aa3b22b79e8aa718
jserweta <jakub.serweta@gmail.com>
2025-04-17 13:45:38 +0200

    tailwind upgrade

--- src/sanity/schemas/singletons/settings.ts
+++ src/sanity/schemas/singletons/settings.ts
@@ -79,7 +79,7 @@ export default defineType({
             // {
             //   type: "page",
             // },
-            { type: "post" },
+            // { type: "post" },
             // {
             //   type: "project",
             // },
