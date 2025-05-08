162933bf081b541278abe841db5b548d90b61d36
jserweta <jakub.serweta@gmail.com>
2025-05-08 17:07:00 +0200

    update sanity config

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -12,14 +12,11 @@ loadEnvConfig(__dirname, dev, {info: () => null, error: console.error});
 // @TODO report top-level await bug
 // Using a dynamic import here as `loadEnvConfig` needs to run before this file is loaded
 // const { projectId, dataset } = await import('@/lib/sanity.api')
-const projectId = process.env.SANITY_STUDIO_PROJECT_ID;
-const dataset = process.env.SANITY_STUDIO_DATASET;
-const studioHost = process.env.SANITY_STUDIO_HOST;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export default defineCliConfig({
-  // project: {basePath: '/studio'},
   api: {projectId, dataset},
-  studioHost: studioHost,
   vite: {
     resolve: {
       alias: {
