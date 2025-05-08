c15bdde2776bced5df3aed1a7e3fb72966ce2697
Jakub Serweta <44199012+jserweta@users.noreply.github.com>
2025-05-08 17:59:59 +0200

    PR - Migration to Next.js (#5)

--- src/client.js
+++
@@ -1,8 +0,0 @@
-import sanityClient from '@sanity/client'
-
-export default sanityClient({
-  projectId: "x37529po",
-  dataset: "production",
-  apiVersion: '2022-02-27',
-  useCdn: true
-})
--- src/components/contact/ValidationBox.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ValidationBox = ({ message, type }) => {
-  return <div className={`validationMessage ${type}`}>{message}</div>;
-};
-
-export default ValidationBox;
